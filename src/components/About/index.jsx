d60ae69790dd7a0ec756edc4a895a730388fdb7e
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-02-17 13:25:06 -0300

    alteração título no about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,7 +10,7 @@ export default function About() {
         </div>
         <div className='about-us about'>
           <h4>Quem sou eu</h4>
-          <h2>Entenda quem sou e por que existo</h2>
+          <h2>Entenda quem sou e qual a minha motivação</h2>
           <p>Meu nome é Thiago Garcia, sou Engenheiro de Controle e Automação e Analista em Energia Solar com mais de 500 usinas homologadas. O meu interesse pela energia solar nasceu ainda na faculdade, através de um trabalho sobre o assunto, onde eu vi que era um tipo de energia que além de ter um caráter ecológico e sustentável muito forte, também tinha custo benefício muito bom. Já a minha história profissional com a energia solar começou lá em 2016, na Alba Energia, onde comecei a trabalhar tanto com os sistemas de aquecimento solar quanto com os sistemas fotovoltaicos. Foi ali que realmente aprendi sobre a energia solar, tanto atravésdos cursose especializações fornecidos pela empresa, quando pela experiência adquirida mais de 170 clientes atendidos</p>
         </div>
       </div>
